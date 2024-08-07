381e58b268200f9500bfe3986cec385b3194545f
yhchen <myself559@gmail.com>
2024-08-07 16:58:54 +0800

    async: 代码调优。同步项目改动 2024/07/31 15:17:39

--- Assets/Scripts/Custom/UIPackage.cs
+++ Assets/Scripts/Custom/UIPackage.cs
@@ -2,7 +2,7 @@ namespace FairyGUI
 {
     public partial class UIPackage
     {
-        // oem: 是否在初始化中
+        // 是否在初始化中
         public static bool IsConstructing => _constructing > 0;
     }
 }
